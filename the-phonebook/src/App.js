da83a49616fcbf3bdd2b8269789d3159b3e4eb7b
SurazGaire <luxheartsuraz@gmail.com>
2022-03-23 13:23:04 +0545

    feat : Added no duplicate entry feature

--- the-phonebook/src/App.js
+++ the-phonebook/src/App.js
@@ -1,45 +1,72 @@
 import { useState } from "react";
 
-const App = () =>{
+const App = () => {
+	const [persons, setPersons] = useState([
+		{
+			name: "Peter Parker",
+			id: 1,
+		},
+	]);
+	const [newName, setNewName] = useState([""]);
 
-  const [persons,setPersons] = useState(
-    [
-      {
-        name:'Peter Parker',
-        id:1
-      }
-    ]
-    )
-    const [newName,setNewName] = useState('my name')
+	const displayText = (e) => {
+		e.preventDefault();
+		setNewName(e.target.value);
+	};
 
-    const displayText = (e)=>{
-      e.preventDefault()
-      setNewName(e.target.value)
-    }
+	const addPerson = () => {
+		// // console.log(newName);
+		// let nameMatch = 0;
+		// for (let i = 0; i < persons.length; i++) {
+		// 	let currElement = persons[i];
+		// 	// existArray.push(currElement.name);
+		// 	// console.log(existArray.indexOf(newName));
+		// 	if (currElement.name === newName) {
+		// 		// console.log("Name already Exist");
+		// 		alert("Name already exists");
+		// 		nameMatch = 1;
+		// 		break;
+		// 	}
+		// }
+		// // let objectString = JSON.stringify(persons);
+		// if (!nameMatch) {
+		// 	// console.log("not exists");
+		// 	const myObject = {
+		// 		name: newName,
+		// 		id: persons.length + 1,
+		// 	};
+		// 	setPersons(persons.concat(myObject));
+		// 	setNewName("");
+		// }
+		let nameMatch = persons.some((person) => person.name === newName);
+		// console.log(nameMatch);
+		if (!nameMatch) {
+			const myObject = {
+				name: newName,
+				id: persons.length + 1,
+			};
+			setPersons(persons.concat(myObject));
+			setNewName("");
+		} else {
+			alert(newName + "is already added to the Phonebook.");
+		}
+	};
 
-    const addPerson = () =>{
-      console.log(newName);
-      const myObject = {
-        name : newName,
-        id : persons.length + 1,
-      }
-      setPersons(persons.concat(myObject))
-      setNewName('')
-    }
-
-  return(
-    <div>
-    <h2>Phonebook</h2>
-        <div>
-          name : <input onChange={displayText} value={newName} />
-        </div>
-        <div>
-          <button onClick={addPerson}>Add</button>
-        </div>
-      <h2>Numbers</h2>
-      {persons.map(person=> <h4 key={person.id}>{person.name}</h4>)}
-    </div>
-  )
-}
+	return (
+		<div>
+			<h2>Phonebook</h2>
+			<div>
+				name : <input onChange={displayText} value={newName} />
+			</div>
+			<div>
+				<button onClick={addPerson}>Add</button>
+			</div>
+			<h2>Numbers</h2>
+			{persons.map((person) => (
+				<h4 key={person.id}>{person.name}</h4>
+			))}
+		</div>
+	);
+};
 
 export default App;
