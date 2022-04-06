40c58ec83cd72299f9ff0bfb3577f1e3fa5fa772
SurazGaire <luxheartsuraz@gmail.com>
2022-04-06 12:56:03 +0545

    feat : Made search work

--- countries-data/src/App.js
+++ countries-data/src/App.js
@@ -2,27 +2,37 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const App = () => {
-	const [countires, setCountries] = useState();
+	const [countries, setCountries] = useState();
 
 	const [searchKey, setSearchKey] = useState("");
 
+	const [resultCountries, setResultCountries] = useState([]);
+
 	useEffect(() => {
 		axios.get("https://restcountries.com/v3.1/all").then((response) => {
-			setCountries(response);
-			console.log(response.data);
+			setCountries(response.data);
+			console.dir(response.data);
 		});
 	}, []);
 
 	const getSearchKey = (event) => {
 		event.preventDefault();
 		setSearchKey(event.target.value);
+
+		const searchResult = countries.filter((country) =>
+			country.name.common.includes(event.target.value)
+		);
+		console.log(searchResult);
+		// setResultCountries(searchResult);
 	};
 
 	return (
 		<div>
 			<div>
-				Find Countries : <input onChange={getSearchKey}></input>
+				Find Countries :{" "}
+				<input value={searchKey} onChange={getSearchKey}></input>
 			</div>
+			<div>{resultCountries}</div>
 		</div>
 	);
 };
