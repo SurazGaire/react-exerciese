d4465a162361184bbc48c166b8c68ba9ba7ffbfa
SurazGaire <luxheartsuraz@gmail.com>
2022-04-07 11:03:25 +0545

    feat : Displayed information using state

--- countries-data/src/App.js
+++ countries-data/src/App.js
@@ -1,17 +1,23 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Country from "./components/Country";
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const App = () => {
 	const [countries, setCountries] = useState();
 
 	const [searchKey, setSearchKey] = useState("");
 
-	const [resultCountries, setResultCountries] = useState([]);
+	const [resultCountries, setResultCountries] = useState([
+		// { name: { common: "Many search Results" } },
+	]);
+
+	const [singleResult, setSingleResult] = useState([]);
 
 	useEffect(() => {
 		axios.get("https://restcountries.com/v3.1/all").then((response) => {
 			setCountries(response.data);
-			console.dir(response.data);
 		});
 	}, []);
 
@@ -23,7 +29,16 @@ const App = () => {
 			country.name.common.includes(event.target.value)
 		);
 		console.log(searchResult);
-		// setResultCountries(searchResult);
+		if (searchResult.length === 1) {
+			setResultCountries(searchResult);
+			setSingleResult(searchResult);
+			console.log("only 1 country");
+		} else if (searchResult.length > 10) {
+			const tooMany = "Too many results,use other filter";
+			setResultCountries([{ name: { common: tooMany } }]);
+		} else {
+			setResultCountries(searchResult);
+		}
 	};
 
 	return (
@@ -32,7 +47,28 @@ const App = () => {
 				Find Countries :{" "}
 				<input value={searchKey} onChange={getSearchKey}></input>
 			</div>
-			<div>{resultCountries}</div>
+			{singleResult.length === 1 ? (
+				<div>
+					<h1>{resultCountries[0].name.common}</h1>
+					<p>{resultCountries[0].capital}</p>
+					<p>{resultCountries[0].area}</p>
+					<h4>Continent :</h4>
+					<ul>
+						{resultCountries.map((continent, index) => (
+							<li key={index}>{continent.continents}</li>
+						))}
+					</ul>
+					<img src={resultCountries[0].flags.png} />
+				</div>
+			) : (
+				<div>
+					<ul>
+						{resultCountries.map((rc, index) => (
+							<Country key={index} country={rc} />
+						))}
+					</ul>
+				</div>
+			)}
 		</div>
 	);
 };
---
+++ countries-data/src/components/Country.js
@@ -0,0 +1,4 @@
+const Country = ({ country }) => {
+	return <li>{country.name.common}</li>;
+};
+export default Country;
